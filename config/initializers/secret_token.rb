d9baf442354c9c8e1536adea5ff5db72950a438a
Arga <Argamasilla@ARGA>
2010-09-30 11:17:23 -0700

    Initial new app rails 3.0-ruby 1.9 framework Signed-off-by: Arga <Argamasilla@ARGA>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '8cd58506f35f94bd5870a95531eaeea14dbc6d327c60a9e34e395d328405ec780da495a14413e71047fa93245ea821a839edcd098111af2af946d2fb4bf8724b'
